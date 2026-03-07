1ae25b60e5592059c7613b68145ca4a0c0d552b3
arsaudax <arsaudax@gmail.com>
2026-03-07 20:32:36 +0300

    fix: use standard RecentNotes for blog index

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,22 +1,131 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
-import BlogIndex from "./quartz-custom/components/BlogIndex"  // Путь должен быть точным
+import { gardenFilter, blogFilter } from "./quartz-custom/utils/filter"
+import * as CustomComponent from "./quartz-custom/components"
+import TagList from "./quartz-custom/components/TagList"
+import { FileTrieNode } from "./quartz/components/scripts/spa"
+
+const explorerConfig = {
+  filterFn: (node: FileTrieNode) => {
+    const hasExcludedTag = node.data?.tags?.includes("explorer-exclude") === true
+    return !hasExcludedTag
+  },
+  mapFn: (node: FileTrieNode) => {
+    if (!node.isFolder) {
+      node.displayName = "⊹ " + node.displayName
+    }
+  },
+}
+
+const graphConfig = {
+  localGraph: { showTags: false, excludeTags: ["graph-exclude"] },
+  globalGraph: { showTags: false, excludeTags: ["graph-exclude"] }
+}
+
+const backlinksConfig = { hideWhenEmpty: true }
+const breadcrumbsConfig = { rootName: "🏡" }
 
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
-  footer: Component.Footer(),
+  footer: CustomComponent.Footer({
+    links: {
+      Telegram: "https://t.me/asteralog",
+      Instagram: "https://www.instagram.com/al.bogat",
+      Behance: "https://www.behance.net/arsaudax"
+    },
+  }),
 }
 
-export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [BlogIndex],
+// Макет для сада
+export const gardenContentPageLayout: PageLayout = {
+  beforeBody: [
+    Component.Breadcrumbs(breadcrumbsConfig),
+    Component.ArticleTitle(),
+    CustomComponent.ContentMeta({ showReadingTime: true }),
+    Component.TagList(),
+  ],
+  left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Search(),
+    Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer(explorerConfig)),
+  ],
+  right: [
+    Component.DesktopOnly(Component.Graph(graphConfig)),
+    Component.DesktopOnly(Component.TableOfContents()),
+    Component.Backlinks(backlinksConfig),
+    Component.RecentNotes({ limit: 5, showTags: false, filter: gardenFilter }),
+    TagList(),
+  ],
+}
+
+// Макет для обычных страниц блога
+export const blogPostPageLayout: PageLayout = {
+  beforeBody: [
+    Component.Breadcrumbs(breadcrumbsConfig),
+    Component.ArticleTitle(),
+    CustomComponent.ContentMeta({ showReadingTime: true }),
+    Component.TagList(),
+  ],
   left: [],
-  right: [],
+  right: [
+    Component.DesktopOnly(Component.TableOfContents()),
+    TagList(),
+    Component.Backlinks(backlinksConfig),
+  ],
 }
 
+// Макет для главной страницы блога с лентой постов
+export const blogIndexPageLayout: PageLayout = {
+  beforeBody: [
+    Component.ArticleTitle(),
+  ],
+  left: [],
+  right: [
+    Component.DesktopOnly(Component.TableOfContents()),
+    // Используем стандартный RecentNotes вместо кастомного BlogIndex
+    Component.RecentNotes({ 
+      title: "Последние записи",
+      limit: 20,
+      showTags: true,
+      filter: blogFilter 
+    }),
+    TagList(),
+    Component.Backlinks(backlinksConfig),
+  ],
+}
+
+// Макет для страниц-списков
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [],
-  left: [],
+  beforeBody: [
+    Component.Breadcrumbs(breadcrumbsConfig),
+    Component.ArticleTitle(),
+    CustomComponent.ContentMeta({ showReadingTime: true }),
+  ],
+  left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Search(),
+    Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer(explorerConfig)),
+  ],
   right: [],
 }
+
+// Условный экспорт
+export const defaultContentPageLayout = (() => {
+  const baseUrl = typeof process !== 'undefined' ? process.env?.BASE_URL : ''
+  const isBlog = baseUrl?.includes('blog')
+  
+  return (props: any) => {
+    if (isBlog) {
+      return props.fileData.slug === 'index' 
+        ? blogIndexPageLayout 
+        : blogPostPageLayout
+    }
+    return gardenContentPageLayout
+  }
+})()

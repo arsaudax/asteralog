8a7e7838c350f42b6baedaff2697e205e4b412c8
arsaudax <arsaudax@gmail.com>
2026-03-10 14:44:41 +0300

    fix: remove non-existent imports from index.ts

--- quartz-custom/components/index.ts
+++ quartz-custom/components/index.ts
@@ -1,19 +1,15 @@
 import ContentMeta from "./ContentMeta"
 import Footer from "./Footer"
-import PageTagList from "./PageTagList" 
 import BlogIndex from "./BlogIndex"
 import PageTitle from "./PageTitle"
 import ArchiveLink from "./ArchiveLink"
-import RightScrollArea from "./RightScrollArea"
-import Head from "./Head"  // ← добавляем кастомный Head
+import Head from "./Head"
 
 export { 
   ContentMeta, 
   Footer, 
-  PageTagList, 
   BlogIndex, 
   PageTitle,
   ArchiveLink,
-  RightScrollArea,
-  Head  // ← экспортируем Head
+  Head
 }
